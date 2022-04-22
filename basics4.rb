237f00c058b6706a651f1ed37dfa1a99d772b498
VSTodd <veronika.todd@gmail.com>
2022-04-22 01:34:36 +0000

    Finished exercises

---
+++ basics4.rb
@@ -0,0 +1,2 @@
+movie_years = [1994, 1997, 2022, 2010]
+puts movie_years
---
+++ basics5.rb
@@ -0,0 +1,9 @@
+five = 5 * 4 * 3 * 2 * 1
+six = 6 * 5 * 4 * 3 * 2 * 1
+seven = 7 * 6 * 5 * 4 * 3 * 2 * 1
+eight = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "Five factoral = #{five}"
+puts "Six factoral = #{six}"
+puts "Seven factoral = #{seven}"
+puts "Eight factoral = #{eight}"
