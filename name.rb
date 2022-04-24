45ff489810f30395c49bed307591df3d584808f9
VSTodd <veronika.todd@gmail.com>
2022-04-24 14:48:22 +0000

    Variables Exercises

---
+++ name.rb
@@ -0,0 +1,10 @@
+#exercise variable #1/4
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "Please enter your last name:"
+last_name = gets.chomp
+name = first_name + " " + last_name
+puts "Hello there, #{name}"
+
+#exercise variable #3
+10.times {puts name}
