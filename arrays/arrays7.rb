5c962303186e6d96544b094b058844573354a3ab
VSTodd <veronika.todd@gmail.com>
2022-05-01 19:10:21 +0000

    Arrays Exercises

---
+++ arrays/arrays7.rb
@@ -0,0 +1,4 @@
+tlk = ["Simba", "Mufasa", "Sarabi", "Nala", "Scar", "Timon", "Pumbaa"]
+tlk.each_with_index do |value, index| 
+  puts "#{index}: #{value}"
+end
---
+++ arrays/arrays8.rb
@@ -0,0 +1,19 @@
+#my method
+arr = [47, -6, 7, 234, -1, 99]
+arr2 = arr.map {|num|num + 2}
+p arr
+p arr2
+
+#other solution
+=begin
+arr = [1, 2, 3, 4, 5]
+new_arr = []
+
+arr.each do |n|
+  new_arr << n + 2
+end
+
+p arr
+p new_arr
+=end
+
